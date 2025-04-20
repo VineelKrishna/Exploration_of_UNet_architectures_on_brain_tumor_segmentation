f72f8e529c65f4d7c4c042a858a54a138a731c07
VamshiNarmety <vamshi3487@gmail.com>
2025-04-20 23:46:45 +0530

    update dataset_format.py

--- src/dataset_format.py
+++ src/dataset_format.py
@@ -1,44 +1,35 @@
 import os
+import cv2
 import glob
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-
+from torch.utils.data import Dataset
+# Dataset
 class TCGADataset(Dataset):
-    def __init__(self, root_dir, transform=None, mask_transform=None):
-        self.root_dir = root_dir
+    def __init__(self, df, transform=None):
+        self.df = df.reset_index(drop=True)
         self.transform = transform
-        self.mask_transform = mask_transform
-        self.image_mask_pairs = []
-
-        # loop over each case folder
-        for case_folder in os.listdir(root_dir):
-            case_path = os.path.join(root_dir, case_folder)
-            if not os.path.isdir(case_path):
-                continue
-
-            # load all .tif images that are not masks
-            images = glob.glob(os.path.join(case_path, '*.tif'))
-            for img_path in images:
-                if img_path.endswith('_mask.tif'):
-                    continue
-                # get the matching mask path
-                mask_path = img_path.replace('.tif', '_mask.tif')
-                if os.path.exists(mask_path):
-                    self.image_mask_pairs.append((img_path, mask_path))
 
     def __len__(self):
-        return len(self.image_mask_pairs)
+        return len(self.df)
 
-    def __getitem__(self, index):
-        img_path, mask_path = self.image_mask_pairs[index]
+    def __getitem__(self, idx):
+        img_path = self.df.loc[idx, 'image_path']
+        mask_path = self.df.loc[idx, 'mask_path']
 
-        image = Image.open(img_path).convert('RGB')
-        mask = Image.open(mask_path).convert('L')  # mask is usually single channel
+        # Read image and mask
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)  # Mask is grayscale
 
+        # Apply transformations
         if self.transform:
-            image = self.transform(image)
-        if self.mask_transform:
-            mask = self.mask_transform(mask)
+            augmented = self.transform(image=image, mask=mask)
+            image = augmented['image']  # Image transformed into tensor
+            mask = augmented['mask']    # Mask transformed into tensor
+            if mask.ndim == 2:  # Ensure mask has a channel dimension
+                mask = mask.unsqueeze(0)
 
         return image, mask
+
